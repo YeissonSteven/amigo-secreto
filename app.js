41a604bc9d59927bafa57aaf982d711212c8656c
stive <stivenordonezv@gmail.com>
2025-02-10 12:51:48 -0500

    crear funcion de agragar amigo

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombresAmigos=[];
-nombresAmigos.push(document.getElementById('amigo')) ;
+
+function agragarAmigos(){
+    if (document.getElementById('amigo') == ""){
+        alert("nombre invalido, por favor introduzca un nombre valido");
+    }else{
+        nombresAmigos.push(document.getElementById('amigo'));
+        document.getElementById("amigo").value+'';
+    }
+    
+}
+
 
