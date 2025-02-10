9955ac44575533f3dbad2ba13a7f923a381ae4f6
stive <stivenordonezv@gmail.com>
2025-02-10 13:00:57 -0500

    correccion de errores en la funcion

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombresAmigos=[];
 
-function agragarAmigos(){
-    if (document.getElementById('amigo') == ""){
+function agregarAmigo(){
+    if (document.getElementById('amigo').value == ""){
         alert("nombre invalido, por favor introduzca un nombre valido");
     }else{
-        nombresAmigos.push(document.getElementById('amigo'));
-        document.getElementById("amigo").value+'';
+        nombresAmigos.push(document.getElementById('amigo').value);
+        document.getElementById("amigo").value='';
     }
-    
+    console.log(nombresAmigos)
 }
 
 
